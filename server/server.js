1fb493282b079661a2071f6462c3cb290fc447f5
David <dgorin6@gatech.edu>
2022-10-23 17:49:52 -0400

    deploy pls

--- server/server.js
+++ server/server.js
@@ -123,11 +123,12 @@ io.on("connection", (socket) => {
     }
   })
 });
-
-app.use(express.static('../client/build'))
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-	});
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'))
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    });
+}
 
 httpServer.listen(5000);
 
