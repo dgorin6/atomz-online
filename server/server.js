74d8143f177414292e91eecf9ae124b26962e70d
David <dgorin6@gatech.edu>
2022-10-23 17:28:14 -0400

    more deploymen

--- server/server.js
+++ server/server.js
@@ -123,13 +123,11 @@ io.on("connection", (socket) => {
     }
   })
 });
-//serve static assets in production
-if(process.env.NODE_ENV === 'production') {
-	//set static folder
-	app.use(express.static('../client/build'))
-	app.get('*', (req, res) => {
+
+app.use(express.static('../client/build'))
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-	})
-}
+	});
+
 httpServer.listen(5000);
 
